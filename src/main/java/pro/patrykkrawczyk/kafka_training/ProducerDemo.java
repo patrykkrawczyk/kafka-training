264c219566ced0bf2e72d1084da25c500d1504bc
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-02-10 01:30:19 +0100

    simple producer

--- src/main/java/pro/patrykkrawczyk/kafka_training/ProducerDemo.java
+++ src/main/java/pro/patrykkrawczyk/kafka_training/ProducerDemo.java
@@ -1,8 +1,30 @@
 package pro.patrykkrawczyk.kafka_training;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
+    private static final String SERVER_ADDRESS = "192.168.0.158:9092";
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVER_ADDRESS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "test-topic";
+        String value = "message";
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, value);
+
+        producer.send(record);
+
+        producer.close();
     }
 }
