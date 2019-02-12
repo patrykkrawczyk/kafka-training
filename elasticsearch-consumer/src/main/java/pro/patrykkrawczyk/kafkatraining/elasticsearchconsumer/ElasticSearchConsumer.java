a8f0d9302692b0d70838779d321174ad3cfe0bb3
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2019-02-12 22:26:34 +0100

    first mock of elastic search consumer

--- elasticsearch-consumer/src/main/java/pro/patrykkrawczyk/kafkatraining/elasticsearchconsumer/ElasticSearchConsumer.java
+++ elasticsearch-consumer/src/main/java/pro/patrykkrawczyk/kafkatraining/elasticsearchconsumer/ElasticSearchConsumer.java
@@ -1,8 +1,56 @@
 package pro.patrykkrawczyk.kafkatraining.elasticsearchconsumer;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexReader;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class ElasticSearchConsumer {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    private static final Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class);
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = createClient();
+
+        String json = "{ \"foo\": \"bar\" }";
+        IndexRequest indexRequest = new IndexRequest("twitter", "tweets")
+                .source(json, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+        logger.info(id);
+
+        client.close();
+    }
+
+    private static RestHighLevelClient createClient() {
+        String hostname = System.getenv("BONSAI_IO_HOSTNAME");
+        String username = System.getenv("BONSAI_IO_USERNAME");
+        String password = System.getenv("BONSAI_IO_PASSWORD");
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient
+                .builder(new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(h -> h.setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(builder);
     }
 }
